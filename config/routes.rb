5f7ac3a3417080264024ea34985edeb0391f0491
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-14 09:53:06 +0000

    [Initial Commit2] bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
-  resources :users, only [:index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit, :destroy]
 end
